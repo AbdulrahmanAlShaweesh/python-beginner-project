c3f6ad587275e6294eec0fce5249b7606205af32
Alshwish Abdulrahman <alshwishabdulrahman@Alshwishs-MacBook-Pro.local>
2023-07-24 15:41:32 +0800

    add class to the game

--- die_game.py
+++ die_game.py
@@ -1,4 +1,22 @@
 
+import random
 
-def DieGame(): 
-    print('hello world')
+class DieGame :
+    
+    def __init__(self, startPlace, endPlace, userPlace, computerPlace, userWins, computerWins) :
+        self.startPlace = startPlace 
+        self.endplace = endPlace 
+        self.userPlace = userPlace 
+        self.computerPlace = computerPlace 
+        self.userWins = userWins 
+        self.computerWins = computerPlace
+    
+    def winer(self) :
+        
+        while not self.userWins or not self.computerWins :
+            user_choose = input('enter t : to toss or q to quit the game ? ').strip().lower() 
+            
+            if user_choose == 'q' :
+                break 
+            elif user_choose == 't' :
+                user_more = random.randint(0,5)
