382fd52082069d0766b3d39aae4e3157283c07a5
Alshwish Abdulrahman <alshwishabdulrahman@Alshwishs-MacBook-Pro.local>
2023-07-24 15:53:18 +0800

    complete winers function

--- die_game.py
+++ die_game.py
@@ -1,22 +1,55 @@
 
-import random
+# import random
+
+# class DieGame :
+    
+#     def __init__(self, startPlace, endPlace, userPlace, computerPlace, userWins, computerWins) :
+#         self.startPlace = startPlace 
+#         self.endplace = endPlace 
+#         self.userPlace = userPlace 
+#         self.computerPlace = computerPlace 
+#         self.userWins = userWins 
+#         self.computerWins = computerPlace
+    
+#     def winer(self) :
+        
+#         while not self.userWins or not self.computerWins :
+#             user_choose = input('enter t : to toss or q to quit the game ? ').strip().lower() 
+            
+#             if user_choose == 'q' :
+#                 break 
+#             elif user_choose == 't' :
+#                 user_more = random.randint(0,5) 
+#                 self.userPlace += user_more 
+                
+#                 if self.userPlace > 40 : 
+#                     over_shot = self.userPlace - 40 
+#                     self.userPlace = 40 -over_shot 
 
 class DieGame :
     
-    def __init__(self, startPlace, endPlace, userPlace, computerPlace, userWins, computerWins) :
+    def __init__(self, startPlace, endPlace, userPlace,computerPlace, isUserWins, isComputerWins) :
         self.startPlace = startPlace 
-        self.endplace = endPlace 
+        self.endPlace = endPlace 
         self.userPlace = userPlace 
         self.computerPlace = computerPlace 
-        self.userWins = userWins 
-        self.computerWins = computerPlace
+        self.isUserWins = isUserWins 
+        self.isComputerWins = isComputerWins
     
-    def winer(self) :
+    def checkWhosWiner(self) :
         
-        while not self.userWins or not self.computerWins :
-            user_choose = input('enter t : to toss or q to quit the game ? ').strip().lower() 
+        while not self.isUserWins or not self.isComputerWins :
+            user_choose = input('do you want to continus (t/q) ? '.capitalize()).strip().lower() 
             
             if user_choose == 'q' :
-                break 
+                print('game over!'.capitalize())
+                break
             elif user_choose == 't' :
-                user_more = random.randint(0,5)
+                pass
+            else :
+                print('invalid user choose'.capitalize())
+
+
+die_game = DieGame(0, 40, 0, 0, False, False) 
+
+die_game.checkWhosWiner()
