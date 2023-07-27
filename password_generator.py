2b48f11706549a899bf2b5cee88ba562ce9fae5e
Alshwish Abdulrahman <alshwishabdulrahman@Alshwishs-MacBook-Pro.local>
2023-07-27 18:49:48 +0800

    finish password generating function

--- password_generator.py
+++ password_generator.py
@@ -1,10 +1,11 @@
-
+import random as r
 class PasswordGenerator : 
     
     def __init__(self): 
         pass 
     
-    # this method generat a symbolm number, uppercase letters and lower cause letters. 
+    # this method generat a symbolm number, uppercase letters and lower cause letters.
+    @property 
     def passwordContains(self) :
         upperCauseLetters = [chr(letter) for letter in range(65,91)]
         lowerCauseLetters = [chr(lowerLetter) for lowerLetter in range(97,123)]
@@ -34,16 +35,29 @@ class PasswordGenerator :
         else : 
             try : 
                 desiredPasswordLength = int(uppercase) + int(lowercase) + int(numberinc) + int(symbols)
+                userPasswordLength = int(userPasswordLength)
                 if userPasswordLength == desiredPasswordLength  : 
                     return int(userPasswordLength),  int(uppercase), int(lowercase), int(numberinc), int(symbols)
                 else : 
+                    print(desiredPasswordLength, userPasswordLength)
                     return 'total password charcketers not match your desirebed passwprd length'
             except :
                 return 'enter the length of the password, only digits allowed.'    
     @property
     def generatPassword(self) :
-        passLength = self.userDesiredPasswordLength
-        upperCauseLetters, lowerCauseLetters, numbers, symbols = self.passwordContains
+        passLength, upperletter, lowerletter, numbers, symbols = self.userDesiredPasswordLength
+        upperCauseLetters, lowerCauseLetters, number, symbol = self.passwordContains
+        newPassword = '' 
+        for i in range(upperletter) :
+            newPassword += r.choice(upperCauseLetters)
+        for i in range(lowerletter): 
+            newPassword += r.choice(lowerCauseLetters)
+        for i in range(numbers)  :
+            newPassword += r.choice(str(number)) 
+        # for j in range(symbols) :
+        #     newPassword += r.choices(symbol)
+        return newPassword 
+         
         
         # if 
         return passLength
@@ -52,5 +66,5 @@ code = [chr(i) for i in range(65, 91)]
 
 password = PasswordGenerator()
 
-# upperCauseLetters, lowerCauseLetters, numbers, symbols = 
-print(password.userDesiredPasswordLength)
+upperCauseLetters, lowerCauseLetters, numbers, symbols = password.passwordContains 
+print(password.generatPassword)
