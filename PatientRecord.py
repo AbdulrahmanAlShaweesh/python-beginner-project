6a498f2b7d5c73912a90194e9836ae782d9aa727
Alshwish Abdulrahman <alshwishabdulrahman@Alshwishs-MacBook-Pro.local>
2023-07-29 19:30:37 +0800

    finish project

--- PatientRecord.py
+++ PatientRecord.py
@@ -1,45 +1,23 @@
 
 
-class PatensRecord : 
+ 
+def displayPationsRecord(**pations) :
+   
+   for names, pationsInfo in pations.items() :
+       
+       for key, value in pationsInfo.items() :
+         print(f'name : {names} \n{key}  : {value}')
+       print()
+
+pations = {
+           'Mohamed': {'age' : 23, 'height' : 1.9, 'weight' : 80}, 
+           'Tom' : {'age' : 29, 'height' : 1.76, 'weight': 89}, 
+           }
+displayPationsRecord(**pations)
     
-    def __ini__(self, ) :
-        # self.pationsPasteRecords = {'mohammed': 12}
-        pass
         
-    def gettingPationsPastRecords(self, **pationsPasteRecords) :
-        
-        self.pationsPasteRecord = pationsPasteRecords # getting pation's records
-    
-    def checkPastionRecord(self) :
-         
-         
-        self.pastionName = input('pation\'s name ? ')    # pation's name.
-        while self.pastionName not in self.pationsPasteRecord.keys() : 
-            print('\n*************************************') 
-            print('add the pastion to database recored')
-            print('*************************************\n')
-            self.addPationToDatabase()
-        else :
-            print('yes')
-            return self.pationsPasteRecord
-    
-    def addPationToDatabase(self) :
-            
-            try :
-                
-                age = int(input('pastion\'s age ? ').strip())
-                country = input('pation\s country ? ').strip()
-                height = float(input('pation\'s heights ? ').strip()) 
-                weight = float(input('pation\'s weights ? ').strip())
-                 
-                self.pationsPasteRecord.update({self.pastionName : {'name' : self.pastionName, 'age': age, 'country': country, 'height' : height, 'weight': weight}})
-                return self.pationsPasteRecord
-            except :
-                return 'something went wrong.'
-pationsPasteRecords = {}
-obj = PatensRecord()
-print(obj.gettingPationsPastRecords(**pationsPasteRecords))
-print(obj.checkPastionRecord())
+
+ 
 
 
         
