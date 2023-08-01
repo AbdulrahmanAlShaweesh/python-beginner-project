6664ae2fb33932f261d7feac3e63fffd7f057814
Alshwish Abdulrahman <alshwishabdulrahman@Alshwishs-MacBook-Pro.local>
2023-08-01 15:37:35 +0800

    working on quation function

--- arithmetic_game.py
+++ arithmetic_game.py
@@ -1,4 +1,40 @@
 import random as r 
 import time as t 
 
-print(dir(t))
+
+class ArithmeticGames : 
+    
+    def __init__(self, number_of_quations) :
+        self.number_of_quations = number_of_quations 
+    
+    # this function is to return the quations and operator. 
+    def numberAndOperator(self):
+        self.number1 = r.randint(1,50) 
+        self.number2 = r.randint(1, 50) 
+        self.operators = r.choice(['+', '-', '/', '//', '*', '%', '**']) 
+        
+        return self.number1, self.number2, self.operators
+    
+    def generateQuation(self) : 
+        if self.operators == '+' : 
+            answer = self.number1 + self.number2 
+        elif self.operators == '-' :
+            answer = self.number1 - self.number2 
+        elif self.operators == '/'  :
+            answer = self.number1 / self.number2 
+        elif self.operators == '//' :
+            answer = self.number1 * self.number2 
+        elif self.operators == '%' : 
+            answer = self.number1 % self.number2 
+        else : 
+            answer = self.number1 ** self.number2
+        return answer
+     
+    # this function display the quation. 
+    def quation(self) : 
+        number1, number2, opertor = self.numberAndOperator()
+        quation = input(f'{number1} {opertor} {number2}? = ')
+        print(quation)
+play_game = ArithmeticGames(10) 
+
+play_game.quation()
